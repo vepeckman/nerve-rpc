7db7cc49c58e0c187f859c09cb279d6c77c79782
Nicolas Peckman <nicolas.peckman@sas.com>
2019-01-30 16:48:42 -0500

    Prep for e2e test

---
+++ tests/api.nim
@@ -0,0 +1,14 @@
+import current
+when not defined(js):
+  import asyncdispatch
+else:
+  import asyncjs
+
+rpc:
+  proc hello*(name: string): Future[string] =
+    result = newFuture[string]()
+    result.complete("Hello " & name)
+  
+  proc add*(x, y: int): Future[int] =
+    result = newFuture[int]()
+    result.complete(x + y)
