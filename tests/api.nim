bcb117ca4c5fb8a6dbae747b7bbcfae344c0f4b2
Nicolas Peckman <nehpeckman@pop-os.localdomain>
2019-05-30 20:57:04 -0400

    Added more tests

--- tests/api.nim
+++ tests/api.nim
@@ -11,18 +11,22 @@ type
     name*: kstring
     age*: int
   Parent* = ref object
-    self: Person
-    children: seq[Person]
+    self*: Person
+    children*: seq[Person]
 
 rpc example, "/api":
-  proc hello(name: kstring): Future[kstring] =
+  proc hello(name = kstring("World")): Future[kstring] =
     result = newFuture[kstring]()
     result.complete("Hello " & name)
   
-  proc add(x, y: int): Future[int] =
+  proc add(x, y = 0): Future[int] =
     result = newFuture[int]()
     result.complete(x + y)
 
   proc newPerson(name: kstring, age: int): Future[Person] =
     result = newFuture[Person]()
     result.complete(Person(name: name, age: age))
+
+  proc newParent(person: Person, child: Person): Future[Parent] =
+    result = newFuture[Parent]()
+    result.complete(Parent(self: person, children: @[child]))
--- tests/client.nim
+++ tests/client.nim
@@ -11,10 +11,27 @@ proc main() {.async.} =
     test "Hello":
       let msg = await example.hello("Nic")
       check(msg == "Hello Nic")
+      let helloWorld = await example.hello()
+      check(helloWorld == "Hello World")
+
+    test "Add":
+      let x = await example.add()
+      let y = await example.add(1)
+      let z = await example.add(2, 3)
+      check(x == 0)
+      check(y == 1)
+      check(z == 5)
 
     test "Person":
       let person = await example.newPerson("Nic", 24)
       check(person.name == "Nic")
+
+    test "Parent":
+      let person = await example.newPerson("Alex", 32)
+      let child = await example.newPerson("James", 4)
+      let parent = await example.newParent(person, child)
+      check(parent.self.name == "Alex")
+      check(parent.children[0].name == "James")
 
 
 discard main()
